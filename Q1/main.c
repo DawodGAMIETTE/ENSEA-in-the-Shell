3a42ca7a5dcd6cd2b5cb40ba09a46d7ad58ce177
DavidGAMIETTE <david.gamiette@gmail.com>
2023-12-07 08:18:59 +0100

    Q1

--- Q1/main.c
+++ Q1/main.c
@@ -4,12 +4,8 @@
 #include <unistd.h>
 
 int main() {
-
     syscall(SYS_write,1,"Bienvenue dans le Shell ENSEA\n",30);
     syscall(SYS_write,1,"Pour quitter, tapez 'exit'.\n",28);
-
-    //printf("Hello, World!\n");
-    //scanf("press exit");
     return 0;
 
 }
