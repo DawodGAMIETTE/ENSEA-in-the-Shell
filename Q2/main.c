73d40bc9c7c13ef4d440fd372286bdcfc4edd4a0
DavidGAMIETTE <david.gamiette@gmail.com>
2023-12-07 12:09:30 +0100

    Q2 a Q4

--- Q2/main.c
+++ Q2/main.c
@@ -9,16 +9,24 @@
 #include <string.h>
 
 int main() {
+    syscall(SYS_write,1,"Bienvenue dans le Shell ENSEA\n",30);
+    syscall(SYS_write,1,"Pour quitter, tapez 'exit'.\n",28);
+
     pid_t ret = fork();
 
     if (ret == 0) {
         char name[20];
         while (1){
+            write(1,"enseash % ", strlen("enseash % "));
             scanf("%s",&name);
             if(strcmp("exit",name)==0){ exit(1);}
             else{
                 pid_t ret2=fork();
+                char* begin;
+                char* end;
                 if (ret2==0){
+                    write(1,name,strlen(name));
+                    write(1,"  ",2);
                     execvp(name, NULL);
                 }
                 else{
@@ -32,4 +40,3 @@ int main() {
     }
     return 0;
 }
-
