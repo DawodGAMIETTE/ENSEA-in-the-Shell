172582e8de9d384545d562848d2c8d95c7de0689
DavidGAMIETTE <david.gamiette@gmail.com>
2023-12-07 10:08:14 +0100

    Q2

--- Q2/main.c
+++ Q2/main.c
@@ -4,21 +4,14 @@
 #include <sys/syscall.h>
 #include <sys/wait.h>
 
-int main() {
-    char *buff = NULL;
-    while()
-    while (1) {
-        read(0,buff,4);
-        write(1,"salut",5);
-        if (buff!=0){
-            syscall(SYS_write,1,&buff,4);
-        }
-        void *buf;
-        //read(-1, buff, 100);
-        //write(0,buff,4);
-
-        //syscall(SYS_read, 1, buff);
-        //write(0,"bon",3);
-        return 0;
+int main(int argc,char *argv[]) {
+    char* command=argv[1];
+    pid_t ret = fork();
+    if (ret == 0) {
+        //write(1, "Je suis le fils\n", strlen("Je suis le fils\n"));
+        execvp(command, NULL);
+    } else {
+        wait(NULL);
     }
+    return 0;
 }
