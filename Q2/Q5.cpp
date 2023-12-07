0dc4193160f58819a879894a2a01bf4a676df45c
DavidGAMIETTE <david.gamiette@gmail.com>
2023-12-07 12:10:48 +0100

    Q5

---
+++ Q2/Q5.cpp
@@ -0,0 +1,47 @@
+//
+// Created by davigami29 on 07/12/23.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <time.h>
+
+int main() {
+    syscall(SYS_write,1,"Bienvenue dans le Shell ENSEA\n",30);
+    syscall(SYS_write,1,"Pour quitter, tapez 'exit'.\n",28);
+
+    pid_t ret = fork();
+
+    if (ret == 0) {
+        char name[20];
+        while (1){
+            write(1,"enseash % ", strlen("enseash % "));
+            scanf("%s",&name);
+            if(strcmp("exit",name)==0){ exit(1);}
+            else{
+                pid_t ret2=fork();
+                char* begin;
+                char* end;
+                clock_gettime(CLOCK_MONOTONIC,&begin);
+                if (ret2==0){
+                    write(1,name,strlen(name));
+                    write(1,"  ",2);
+                    clock_gettime(CLOCK_MONOTONIC,&end);
+                    long time=end-begin;
+                    write(1,time, strlen(time));
+                    execvp(name, NULL);
+                }
+                else{
+                    wait(NULL);
+                }
+            }
+            wait(NULL);
+        }
+    } else {
+        wait(NULL);
+    }
+    return 0;
+}
